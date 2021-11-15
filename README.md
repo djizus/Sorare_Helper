0f8e61aeb0e5fba88a875b827725640af33a9c59
djizus <mehrcj@gmail.com>
2021-11-15 11:26:18 +0100

    Posting v0.4 of the script.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Sorare_Helper
-Identify bargains
+
+Sorare Data is currently the best tool to scout players for Sorare.
+
+This TamperMonkey script is meant to enhance the Sorare Data experience. You can install the script following the instructions below.
+
+## Features
+- [x] Mark bargains (Offer < Market price)
+
+## Installation
+Make sure you have user scripts enabled in your browser (these instructions refer to the latest versions of the browser):
+
+* Firefox - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=firefox). :warning: Has issues loading properly (see issue #115)
+* Chrome - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=chrome).
+* Opera - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=opera).
+* Safari - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=safari).
+* Dolphin - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=dolphin).
+* UC Browser - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=ucweb).
+
+### Install scripts
+Install the scripts via [OpenUserJS][install-script]. Or find the latest version and release notes at the [releases page](https://github.com/djizus/SorareData_Helper/releases).
+
+## Feature requests, Issues and Contribution
+I don't plan to support this script regularly, you can still post in Issues and I may look into it.
+
+[issue-list]: https://github.com/djizus/SorareData_Helper/issues
+[install-script]: https://openuserjs.org/install/djizus/SorareData_Helper.js
---
+++ SorareData_Helper.js
@@ -0,0 +1,127 @@
+// ==UserScript==
+// @name		SorareData Helper
+// @namespace	http://tampermonkey.net/
+// @version		0.4
+// @description	Helps you identify the bargains
+// @author		Djizus
+// @include		/^(?:https?:\/\/)?(?:www\.)?soraredata.com\/publicOffers/
+// @include		/^(?:https?:\/\/)?(?:www\.)?soraredata.com\/ongoingAuctions/
+// @icon		https://www.google.com/s2/favicons?domain=soraredata.com
+// @grant		GM_getValue
+// @grant		GM_setValue
+// @grant		GM_addStyle
+// @grant		GM_registerMenuCommand
+// @require		https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @require		http://code.jquery.com/jquery-3.4.1.min.js
+// @license		MIT
+// ==/UserScript==
+
+(function() {
+    "use strict";
+	
+    window.addEventListener("load", () => {
+		quickCheckOffers();
+    });
+
+    function quickCheckOffers() {
+        $('.infinite-scroll-component > div > div').each(function(index) {
+
+            let box = $(this);
+            let infoBox = box.find('.iMQCHk');
+            // get values
+            let ethValues = infoBox.children("div:first")
+            let ethVal = ethValues.find('div:eq(1) p').text();
+			let leftValue = 0.000001;
+
+            let num = ethVal.match(/[\d\.]+/g);
+            let ethValNum = parseFloat(num[0]);
+
+            let bestMarketPrice = ethValues.find('div[data-tip="Best market price"] p').text();
+            let bestMarketPriceNum = bestMarketPrice.match(/[\d\.]+/g);
+            if (bestMarketPriceNum == null) {
+                bestMarketPriceNum = 0.000001;
+            } else if (bestMarketPriceNum.length > 1 ){
+                bestMarketPriceNum = parseFloat(bestMarketPriceNum[1]);
+            } else {
+				bestMarketPriceNum = parseFloat(bestMarketPriceNum[0]);
+			}
+
+            let Average3Days = ethValues.find('div[data-tip="3 days average"] p').text();
+            let Average3DaysNum = Average3Days.match(/[\d\.]+/g);    
+			let Average1Week = ethValues.find('div[data-tip="1 week average"] p').text();
+            let Average1WeekNum = Average1Week.match(/[\d\.]+/g);
+			let Average2Weeks = ethValues.find('div[data-tip="2 weeks average"] p').text();
+            let Average2WeeksNum = Average2Weeks.match(/[\d\.]+/g);
+            let Average1Month = ethValues.find('div[data-tip="1 month average"] p').text();
+            let Average1MonthNum = Average1Month.match(/[\d\.]+/g);
+            let lifetimePriceAverage = ethValues.find('div[data-tip="Card lifetime price average"] p').text();
+            let lifetimePriceAverageNum = lifetimePriceAverage.match(/[\d\.]+/g);        
+			
+            if (Average3DaysNum == null) {
+				if(Average1WeekNum == null) {
+					if(Average2WeeksNum == null) {
+						if(Average1MonthNum == null) {
+							if(lifetimePriceAverageNum == null) {
+								leftValue = parseFloat(bestMarketPriceNum[0]);
+							}
+							else{
+								leftValue = parseFloat(lifetimePriceAverageNum[0]);
+							}
+						}
+						else{
+							leftValue = parseFloat(Average1MonthNum[0]);
+						}
+					}
+					else {
+						leftValue = parseFloat(Average2WeeksNum[0]);					
+					}
+				}
+				else{
+					leftValue = parseFloat(Average1WeekNum[0]);
+				}					
+            } else {
+                leftValue = parseFloat(Average3DaysNum[0]);
+            }
+
+            //percent 5 games.
+            let pointValues = infoBox.find('.eZKQvz');
+            let percent5Game = pointValues.find("span[data-tip='% of games played over the past 5 games']").text();
+            let percent5GameNum = percent5Game.match(/[\d\.]+/g);
+            percent5GameNum = percent5GameNum && percent5GameNum[0]?parseInt(percent5GameNum[0]):0;
+			
+            //points 5 games.
+            let points5Game = pointValues.find("span[data-tip='Average score over the past 5 games']").text();
+            let points5GameNum = points5Game.match(/[\d\.]+/g);
+            points5GameNum = points5GameNum && points5GameNum[0]?parseInt(points5GameNum[0]):0;
+
+            //percent 15 games.
+            let percent15Game = pointValues.find("span[data-tip='% of games played over the past 15 games']").text();
+            let percent15GameNum = percent15Game.match(/[\d\.]+/g);
+            percent15GameNum = percent15GameNum && percent15GameNum[0]?parseInt(percent15GameNum[0]):0;
+
+            //points 15 games.
+            let points15Game = pointValues.find("span[data-tip='Average score over the past 15 games']").text();
+            let points15GameNum = points15Game.match(/[\d\.]+/g);
+            points15GameNum = points15GameNum && points15GameNum[0]?parseInt(points15GameNum[0]):0;
+			
+            if (ethValNum <= leftValue && percent15GameNum > 0) {
+                box.css('border', '5px solid Blue');
+            } else if (ethValNum / bestMarketPriceNum <= 0.9 && percent15GameNum > 9) {
+                box.css('border', '5px solid Lime');
+            } else if (ethValNum / bestMarketPriceNum <= 0.95 && percent15GameNum > 9) {
+                box.css('border', '5px solid Gold');
+            } else if (ethValNum / bestMarketPriceNum <= 1 && percent15GameNum > 9) {
+                box.css('border', '5px solid Peru');
+            }
+            else {
+                //box.css('border', '5px solid grey');
+                box.css('display', 'none');
+            }
+        });
+    }
+
+    setInterval(function(){
+        quickCheckOffers();
+    }, 500);
+
+})();
